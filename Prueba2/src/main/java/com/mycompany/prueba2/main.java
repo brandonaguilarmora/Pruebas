ebc2e39897c1e5cc9537c4387712a7d87eff05a1
brandonaguilarmora <brandonam2017@hotmail.com>
2022-02-11 14:46:28 -0500

    no pasa nada

--- Prueba2/src/main/java/com/mycompany/prueba2/main.java
+++ Prueba2/src/main/java/com/mycompany/prueba2/main.java
@@ -2,16 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.ejercicio;
+package com.mycompany.prueba2;
 
 /**
  *
  * @author home
  */
 public class main {
-    public static void main(String[] args){
-        System.out.println("Hola Mundo");
-        System.out.println("Chao Mundo");
-        int a=0;
-    }
+    
 }
