8ffb6d7b5b106a6fbec56130d98d5c189ed6c977
brandonaguilarmora <brandonam2017@hotmail.com>
2022-02-11 18:57:19 -0500

    no cambie una monda 5

--- ejercicio/src/main/java/com/mycompany/ejercicio/main.java
+++ ejercicio/src/main/java/com/mycompany/ejercicio/main.java
@@ -12,7 +12,6 @@ public class main {
     public static void main(String[] args){
         System.out.println("Hola Mundo");
         System.out.println("Chao Mundo");
-        int a=0;
-        System.out.println(a);
+        int a=0; System.out.println(a);
     }
 }
